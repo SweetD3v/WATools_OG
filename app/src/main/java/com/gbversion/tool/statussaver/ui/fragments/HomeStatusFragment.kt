13d79d1b88c8c4c263f3cfab9560823ce6fed645
Dev4Life <33550494+SweetD3v@users.noreply.github.com>
2023-01-14 01:04:00 +0530

    gg

--- app/src/main/java/com/gbversion/tool/statussaver/ui/fragments/HomeStatusFragment.kt
+++ app/src/main/java/com/gbversion/tool/statussaver/ui/fragments/HomeStatusFragment.kt
@@ -1,8 +1,20 @@
 package com.gbversion.tool.statussaver.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Parcelable
+import android.os.storage.StorageManager
+import android.provider.DocumentsContract
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -18,86 +30,138 @@ class HomeStatusFragment : BaseFragment<FragmentHomeStatusBinding>(), WATypeChan
         return FragmentHomeStatusBinding.inflate(layoutInflater)
     }
 
+    private val PERMISSIONS = arrayOf(
+        "android.permission.READ_EXTERNAL_STORAGE",
+        "android.permission.WRITE_EXTERNAL_STORAGE"
+    )
+
+    val permissionsLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions(),
+            object : ActivityResultCallback<Map<String, Boolean>> {
+                @RequiresApi(Build.VERSION_CODES.M)
+                override fun onActivityResult(result: Map<String, Boolean>?) {
+                    var granted = true
+                    if (result != null) {
+                        for (b in result.values) {
+                            if (!b) {
+                                granted = false
+                                break
+                            }
+                        }
+                    } else granted = false
+
+                    if (granted) {
+                        setupViewPager()
+                    }
+                }
+            })
+
+    val statusFileResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                val data: Intent = result.data!!
+                Log.d("HEY: ", data.data.toString())
+                ctx.contentResolver.takePersistableUriPermission(
+                    data.data!!,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                )
+                Toast.makeText(ctx, "Success", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     companion object {
         open fun newInstance(): HomeStatusFragment {
             return HomeStatusFragment()
         }
     }
 
-    private val tabTitles = arrayOf("Photos", "Videos")
-
-    lateinit var imgFragment: WAImagesFragment
-//    lateinit var vidFragment: WAVideosFragment
-//    lateinit var savedFragment: WADownloadsFragment
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.run {
-            if (NetworkState.isOnline()) {
-                AdsUtils.loadNative(
-                    ctx,
-                    getString(R.string.admob_native_id),
-                    adFrame
-                )
-            }
-        }
-
+        Log.e("TAG", "onResume: " + arePermissionDenied())
+//        if (Build.VERSION.SDK_INT >= 23 && arePermissionDenied()) {
+//            if (Build.VERSION.SDK_INT >= 29) {
+//                openDocTreeStatus()
+//            } else {
+//                permissionsLauncher.launch(PERMISSIONS)
+//            }
+//        } else {
+//            AppUtils.APP_DIR = ctx.getExternalFilesDir(getString(R.string.app_name))!!.path
+//            Log.d("App Path", AppUtils.APP_DIR.toString())
+//            setupViewPager()
+//        }
         setupViewPager()
     }
 
     private fun setupViewPager() {
-        binding.apply {
-            imgFragment = WAImagesFragment.newInstance()
-//            vidFragment = WAVideosFragment.newInstance()
-//            savedFragment = WADownloadsFragment.newInstance()
-
-            try {
-                viewPagerStatus.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-                viewPagerStatus.adapter = FragmentsAdapter(requireActivity())
-
-//                TabLayoutMediator(tabLayout, viewPagerStatus) { tab, position ->
-//                    tab.text = tabTitles[position]
-//                }.attach()
-            } catch (e: Exception) {
-            }
+        binding.run {
+            viewPagerStatus.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            viewPagerStatus.adapter = FragmentsAdapter(requireActivity())
+            viewPagerStatus.registerOnPageChangeCallback(object :
+                ViewPager2.OnPageChangeCallback() {
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+                }
+            })
         }
     }
 
+    private fun arePermissionDenied(): Boolean {
+        if (Build.VERSION.SDK_INT >= 29) {
+            return ctx.contentResolver.persistedUriPermissions.size <= 0
+        }
+        for (str in PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(ctx, str) != 0) {
+                return true
+            }
+        }
+        return false
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun openDocTreeStatus() {
+        Log.e("TAG", "requestPermissionQ: ")
+        val createOpenDocumentTreeIntent =
+            (ctx.getSystemService(AppCompatActivity.STORAGE_SERVICE) as StorageManager).primaryStorageVolume.createOpenDocumentTreeIntent()
+        val replace: String =
+            (createOpenDocumentTreeIntent.getParcelableExtra<Parcelable>(DocumentsContract.EXTRA_INITIAL_URI) as Uri?).toString()
+                .replace("/root/", "/document/")
+        val parse: Uri =
+            Uri.parse("$replace%3AAndroid%2Fmedia%2Fcom.whatsapp%2FWhatsApp%2FMedia%2F.Statuses")
+        Log.d("URI", parse.toString())
+        createOpenDocumentTreeIntent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, parse)
+        statusFileResultLauncher.launch(createOpenDocumentTreeIntent)
+    }
+
+
     inner class FragmentsAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
         override fun getItemCount(): Int {
-            return 1
+            return 2
         }
 
         override fun createFragment(position: Int): Fragment {
-            return when (position) {
+            when (position) {
                 0 -> {
-                    imgFragment
+                    return WAImagesFragment.newInstance()
                 }
-                else -> imgFragment
+                1 -> {
+                    return WAVideosFragment.newInstance()
+                }
             }
+            return WAImagesFragment.newInstance()
         }
     }
 
     override fun onBackPressed() {
-        requireActivity().onBackPressed()
     }
 
     override fun onTypeChanged(type: Int) {
-        if (type == 0)
-            imgFragment.onTypeChanged(type)
-        else {
-            var openPicker = true
-            for (uriPermission in ctx.contentResolver.persistedUriPermissions) {
-                Log.e("TAG", "onTypeChanged: ${uriPermission.uri.toString()}")
-                if (uriPermission.uri.toString().contains(".w4b")) {
-                    openPicker = false
-                    break
-                }
-            }
-
-            if (!openPicker)
-                imgFragment.onTypeChanged(type)
-        }
     }
 }
